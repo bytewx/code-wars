4c30e224ff1e6b3bf8bc4aceb6576e67d2e8ecc1
byteshell <satymbayevt@gmail.com>
2024-10-29 17:55:21 +0500

    Create Basic Mathematical Operations.cs

---
+++ C#/8-kyu/Basic Mathematical Operations.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace Solution
+{
+  public static class Program
+  {
+    public static double basicOp(char operation, double value1, double value2)
+    {
+      switch (operation)
+      {
+        case '+':
+          return value1 + value2;
+        case '-':
+          return value1 - value2;
+        case '*':
+          return value1 * value2;
+        case '/':
+          return value1 / value2;
+        default:
+          throw new ArgumentException("Invalid operation");
+      }
+    }
+  }
+}
