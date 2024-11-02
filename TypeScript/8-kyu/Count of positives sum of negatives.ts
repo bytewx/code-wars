b02158d00433250fbf279a961c066d68fd6ac3fc
byteshell <satymbayevt@gmail.com>
2024-11-02 08:16:17 +0500

    Create Count of positives sum of negatives.ts

---
+++ TypeScript/8-kyu/Count of positives sum of negatives.ts
@@ -0,0 +1,13 @@
+export function countPositivesSumNegatives(input: any): number[] {
+  if (!input || input.length === 0) return [];
+
+  let countPositives = 0;
+  let sumNegatives = 0;
+
+  for (let num of input) {
+    if (num > 0) countPositives++;
+    else if (num < 0) sumNegatives += num;
+  }
+
+  return [countPositives, sumNegatives];
+}
