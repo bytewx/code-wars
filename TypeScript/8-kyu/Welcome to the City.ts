f89e53c432b93ba3d1d19f071a1d2c3ee10ee7f6
byteshell <satymbayevt@gmail.com>
2024-11-02 10:36:46 +0500

    Create Welcome to the City.ts

---
+++ TypeScript/8-kyu/Welcome to the City.ts
@@ -0,0 +1,4 @@
+export const sayHello = (name: string[], city: string, state: string): string => {
+  const fullName = name.join(' ');
+  return `Hello, ${fullName}! Welcome to ${city}, ${state}!`;
+};
