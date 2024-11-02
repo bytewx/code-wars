fdc86a17b619119ae9f0ced560c47d5f099db746
byteshell <satymbayevt@gmail.com>
2024-11-02 10:08:08 +0500

    Create Drink about.ts

---
+++ TypeScript/8-kyu/Drink about.ts
@@ -0,0 +1,11 @@
+export function peopleWithAgeDrink(age: number): string {
+    if (age < 14) {
+        return "drink toddy";
+    } else if (age < 18) {
+        return "drink coke";
+    } else if (age < 21) {
+        return "drink beer";
+    } else {
+        return "drink whisky";
+    }
+}
