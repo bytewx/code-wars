e1d63e805309f8495012df5111878f524bf6188f
byteshell <satymbayevt@gmail.com>
2024-11-02 09:43:15 +0500

    Create Thinkful - Logic Drills: Traffic light.ts

---
+++ TypeScript/8-kyu/Thinkful - Logic Drills: Traffic light.ts
@@ -0,0 +1,12 @@
+export function updateLight(current: string): string {
+  switch (current) {
+    case 'green':
+      return 'yellow';
+    case 'yellow':
+      return 'red';
+    case 'red':
+      return 'green';
+    default:
+      return current;
+  }
+}
