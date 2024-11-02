afd47d0c397a223c4457b68767b3b4ff29a5f706
byteshell <satymbayevt@gmail.com>
2024-11-02 09:48:01 +0500

    Create Find Multiples of a Number.ts

---
+++ TypeScript/8-kyu/Find Multiples of a Number.ts
@@ -0,0 +1,9 @@
+export function findMultiples(integer: number, limit: number): number[] {
+  const multiples: number[] = [];
+  
+  for (let i = integer; i <= limit; i += integer) {
+    multiples.push(i);
+  }
+  
+  return multiples;
+}
