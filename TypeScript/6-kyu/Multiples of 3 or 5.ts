46669dfa556b1a61289cf673021de539ecae8089
byteshell <satymbayevt@gmail.com>
2024-11-02 09:52:24 +0500

    Create Multiples of 3 or 5.ts

---
+++ TypeScript/6-kyu/Multiples of 3 or 5.ts
@@ -0,0 +1,17 @@
+export class Challenge {
+  static solution(number: number) {
+    if (number < 0) {
+      return 0;
+    }
+    
+    let sum = 0;
+    
+    for (let i = 0; i < number; i++) {
+      if (i % 3 === 0 || i % 5 === 0) {
+        sum += i;
+      }
+    }
+    
+    return sum;
+  }
+}
