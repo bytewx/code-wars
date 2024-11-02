c4ec79d6a30f6d6c34092b7ce5138745438e5b6a
byteshell <satymbayevt@gmail.com>
2024-11-02 10:18:29 +0500

    Create Check the exam.ts

---
+++ TypeScript/7-kyu/Check the exam.ts
@@ -0,0 +1,15 @@
+export function checkExam(array1: string[], array2: string[]): number {
+    let score = 0;
+
+    for (let i = 0; i < array1.length; i++) {
+        if (array2[i] === "") {
+            score += 0;
+        } else if (array2[i] === array1[i]) {
+            score += 4; 
+        } else {
+            score -= 1; 
+        }
+    }
+
+    return score < 0 ? 0 : score;
+}
