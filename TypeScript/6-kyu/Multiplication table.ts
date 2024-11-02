112d158adee76817ff89db069c53ebb3f01d9964
byteshell <satymbayevt@gmail.com>
2024-11-02 10:19:44 +0500

    Create Multiplication table.ts

---
+++ TypeScript/6-kyu/Multiplication table.ts
@@ -0,0 +1,13 @@
+export function multiplicationTable(size: number): number[][] {
+    const table: number[][] = []; 
+
+    for (let i = 1; i <= size; i++) {
+        const row: number[] = []; 
+        for (let j = 1; j <= size; j++) {
+            row.push(i * j); 
+        }
+        table.push(row); 
+    }
+
+    return table; 
+}
