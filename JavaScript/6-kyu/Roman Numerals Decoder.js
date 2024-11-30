21af11ca7bd51e960a856849c57b1853cf42b8e8
byteshell <satymbayevt@gmail.com>
2024-12-01 00:27:04 +0500

    Create Roman Numerals Decoder.js

---
+++ JavaScript/6-kyu/Roman Numerals Decoder.js
@@ -0,0 +1,26 @@
+function solution(roman) {
+  const romanValues = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000
+  };
+
+  let total = 0;
+
+  for (let i = 0; i < roman.length; i++) {
+    const current = romanValues[roman[i]];
+    const next = romanValues[roman[i + 1]];
+
+    if (current < next) {
+      total -= current;
+    } else {
+      total += current;
+    }
+  }
+
+  return total;
+}
