1ef207e87437d797dd275a89f51f313f0d8e3b64
byteshell <satymbayevt@gmail.com>
2024-12-19 22:45:54 +0500

    Create Anagram Detection.js

---
+++ JavaScript/7-kyu/Anagram Detection.js
@@ -0,0 +1,6 @@
+var isAnagram = function(test, original) {
+    const sortedTest = test.toLowerCase().split('').sort().join('');
+    const sortedOriginal = original.toLowerCase().split('').sort().join('');
+    
+    return sortedTest === sortedOriginal;
+};
