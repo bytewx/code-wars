d6666dcae2f825b5c88d165af1675501b02aadf5
byteshell <satymbayevt@gmail.com>
2024-12-25 10:23:44 +0500

    Create Factorial.js

---
+++ JavaScript/7-kyu/Factorial.js
@@ -0,0 +1,12 @@
+function factorial(n) {
+  if (n < 0 || n > 12) {
+    throw new RangeError("Input must be between 0 and 12 inclusive.");
+  }
+  
+  let result = 1;
+  for (let i = 1; i <= n; i++) {
+    result *= i;
+  }
+  
+  return result;
+}
