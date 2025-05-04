3316043e7612db6c477107ff81ed0aaf7002e8f0
byteshell <satymbayevt@gmail.com>
2025-05-04 14:55:26 +0500

    Create Cartesian neighbors.go

---
+++ Go/7-kyu/Cartesian neighbors.go
@@ -0,0 +1,16 @@
+package kata
+
+func CartesianNeighbor(x, y int) [][]int {
+    neighbors := [][]int{}
+
+    for dx := -1; dx <= 1; dx++ {
+        for dy := -1; dy <= 1; dy++ {
+            if dx == 0 && dy == 0 {
+                continue
+            }
+            neighbors = append(neighbors, []int{x + dx, y + dy})
+        }
+    }
+
+    return neighbors
+}
